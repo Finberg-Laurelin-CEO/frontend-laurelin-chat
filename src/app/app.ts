9eac95934722c1bd722ae95c6c902fb0291d587a
Gerard Geer <gerardgeer@Gerards-MacBook-Air.local>
2025-10-16 16:18:12 -0400

    Cool the chat pane is functional.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LaurelinChatStartup } from './components/laurelin-chat-startup/laurelin-chat-startup';
 import { CommonModule } from '@angular/common';
 import { LaurelinChatEntry } from './components/laurelin-chat-entry/laurelin-chat-entry';
+import { LaurelinChatPane } from './components/laurelin-chat-pane/laurelin-chat-pane';
 
 @Component({
   selector: 'app-root',
-  imports: [LaurelinChatStartup, LaurelinChatEntry, CommonModule],
+  imports: [LaurelinChatStartup, LaurelinChatEntry, LaurelinChatPane, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/laurelin-chat-pane/laurelin-chat-pane.ts
+++ src/app/components/laurelin-chat-pane/laurelin-chat-pane.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LaurelinChatEntry } from '../laurelin-chat-entry/laurelin-chat-entry';
+import { CommonModule } from '@angular/common';
+
+class ChatEntry {
+  ts: number = -1;
+  sent: boolean = true;
+  msg: string = 'Default text.';
+
+  constructor(ts: number=-1, sent: boolean=true, msg: string='Default text.') {
+    this.ts = ts;
+    this.sent = sent;
+    this.msg = msg;
+  }
+}
 
 @Component({
-  selector: 'app-laurelin-chat-pane',
-  imports: [],
+  selector: 'laurelin-chat-pane',
+  imports: [CommonModule, LaurelinChatEntry],
   templateUrl: './laurelin-chat-pane.html',
   styleUrl: './laurelin-chat-pane.css'
 })
-export class LaurelinChatPane {
+export class LaurelinChatPane implements OnInit{
+  
+  entries: ChatEntry[] = [];
 
+  constructor() {
+    for ( let i = 0; i < 30; ++i ) {
+      if ( i % 2 == 0 ) {
+        this.entries.push(new ChatEntry())
+      }
+      else {
+        this.entries.push(new ChatEntry(-1, false, 'default text'));
+      }
+    }
+  }
+  ngOnInit(): void {}
+
+  
 }
