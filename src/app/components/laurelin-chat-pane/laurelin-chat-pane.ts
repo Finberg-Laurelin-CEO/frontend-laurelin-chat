0b5f31880804be8f8180ba30cb48dc44871da791
Gerard Geer <gerardgeer@Gerards-MacBook-Air.local>
2025-10-16 14:37:06 -0400

    initial component creation for the chat history pane,.

---
+++ src/app/components/laurelin-chat-pane/laurelin-chat-pane.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laurelin-chat-pane',
+  imports: [],
+  templateUrl: './laurelin-chat-pane.html',
+  styleUrl: './laurelin-chat-pane.css'
+})
+export class LaurelinChatPane {
+
+}
