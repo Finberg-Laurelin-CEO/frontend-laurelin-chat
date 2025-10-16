76c98c74102620446ba03d5e276294b21fb33625
Gerard Geer <gerardgeer@Gerards-MacBook-Air.local>
2025-10-16 14:36:21 -0400

    Further testing,.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LaurelinChatStartup } from './components/laurelin-chat-startup/laurelin-chat-startup';
 import { CommonModule } from '@angular/common';
+import { LaurelinChatEntry } from './components/laurelin-chat-entry/laurelin-chat-entry';
 
 @Component({
   selector: 'app-root',
-  imports: [LaurelinChatStartup, CommonModule],
+  imports: [LaurelinChatStartup, LaurelinChatEntry, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
