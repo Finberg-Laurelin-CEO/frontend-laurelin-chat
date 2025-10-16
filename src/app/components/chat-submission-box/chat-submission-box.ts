a4b59b260a59177523df9ca10334e7ee471eabcb
Gerard Geer <gerardgeer@Gerards-MacBook-Air.local>
2025-10-16 16:26:11 -0400

    Initial commit of chat submission box.

---
+++ src/app/components/chat-submission-box/chat-submission-box.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-submission-box',
+  imports: [],
+  templateUrl: './chat-submission-box.html',
+  styleUrl: './chat-submission-box.css'
+})
+export class ChatSubmissionBox {
+}
