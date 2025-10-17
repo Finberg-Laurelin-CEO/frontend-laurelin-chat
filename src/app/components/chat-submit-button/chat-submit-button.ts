a07c916e7328f3cdc0c6d673fbf2f93cd9836d27
Gerard Geer <gerardgeer@Gerards-MacBook-Air.local>
2025-10-17 12:41:16 -0400

    Initial commit of submit button.

---
+++ src/app/components/chat-submit-button/chat-submit-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-submit-button',
+  imports: [],
+  templateUrl: './chat-submit-button.html',
+  styleUrl: './chat-submit-button.css'
+})
+export class ChatSubmitButton {
+
+}
